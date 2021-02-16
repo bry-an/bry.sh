9526e491b3e29f184a10aaacb2f35b13068e8c2f
Bryan Yunis <bryany@booj.com>
2021-02-15 22:31:39 -0700

    add form, click handler, and axios

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,4 +1,11 @@
+import axios from axios
 const main = () => {
+    const input = document.getElementById('url-field')
+    const button = document.getElementById('submit-button')
+    button.addEventListener('click', () => {
+        console.log('click', input.value)
+        axios.get() // todo
+    })
     console.log('script running')
 }
 
